6d0e7716fa608fc35b53baa97f02874c3437bfbf
Abhishek jain <abhi.jain0295@gmail.com>
2017-01-02 23:06:59 +0530

    simple program to use calculate literate and illiterate people
    calculation literate and illiterate people according to given data and
    given criteria

---
+++ Population.java
@@ -0,0 +1,13 @@
+class Population{
+public static void main(String args[])
+{
+
+int pop=80000;
+int lit=((52*pop)/100);
+System.out.println("total number of literate people are:"+" "+lit );
+int ilit=((35*pop)/100);
+System.out.println("total number of illiterate people are:"+" "+ilit );
+
+
+}
+}

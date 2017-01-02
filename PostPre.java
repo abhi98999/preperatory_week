2369c60f9634c76f732fbff578fc036695c2ee9d
Abhishek jain <abhi.jain0295@gmail.com>
2017-01-02 23:01:04 +0530

    WAP to implement postfix and prefix Operator
    implementiono of postfix and prefix Operator

---
+++ PostPre.java
@@ -0,0 +1,16 @@
+class PostPre{
+
+
+public static void main(String args[])
+{
+int n=5;
+
+System.out.println("number ="+n);
+System.out.println("using prefix operator");
+
+
+System.out.println("value aftr using prefix operator on number"+(++n) );
+System.out.println("value aftr using postfix opertor on number"+(n++));
+
+}
+}
